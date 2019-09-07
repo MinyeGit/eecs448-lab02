465e6c7f819febee03a4109f20cfb8c1c770a200
minye.wu <minye.wu.cs@gmail.com>
2019-09-07 17:06:30 -0500

    Fix the size() method in LinkedList class

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -32,7 +32,7 @@ int LinkedList<T>::size() const
 	/** TODO 
 		Fix this method
 	*/
-	return(0);
+	return m_size;
 }
 
 template <typename T>
@@ -44,7 +44,6 @@ bool LinkedList<T>::search(T value) const
 	/** TODO 
 		Fix this method
 	*/
-
 	return(isFound);
 }
 
@@ -106,7 +105,6 @@ bool LinkedList<T>::removeBack()
 	/** TODO 
 		Fix this method
 	*/
-
 	return(isRemoved);
 }	
 
